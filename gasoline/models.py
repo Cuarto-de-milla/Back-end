79569d71935e9bf4d95a86d328346b2d9f39cecd
Andres Cendales <andres01660@gmail.com>
2020-10-20 07:50:38 -0500

    [DOCS]: Include docstrings and helptext in models

--- gasoline/models.py
+++ gasoline/models.py
@@ -1,53 +1,57 @@
+"""Gasoline models """
+
+# Django
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Stations(models.Model):
+    """ Stations Model"""
     name = models.CharField(max_length=200)
+    
+    # Location
     latitude = models.FloatField(max_length=9)
     longitude = models.FloatField(max_length=9)
-
-    register = models.CharField(max_length=64)
-    active = models.BooleanField(default=True)
-    status = models.CharField(max_length=10,default='ghost')
+    
+    # Status
+    is_active = models.BooleanField(default=True)
+    status = models.CharField(
+        max_length=10,
+        default='ghost',
+        help_text="Station' status based on their activity. It changes when a user verify the station "
+        )
 
     def __str__(self):
+        """Return station name"""
         return self.name
 
 class Prices(models.Model):
+    """ Prices Model
 
-    station_id = models.ForeignKey(Stations, on_delete=models.PROTECT)
+    Model that holds the prices of each type of gasoline by station.
+    """
+    station = models.ForeignKey(Stations, on_delete=models.PROTECT)
 
-    date = models.DateField()
-    magna_price = models.FloatField(max_length=5)
-    premium_price = models.FloatField(max_length=5)
-    diesel_price = models.FloatField(max_length=5)
-
-    def __str__(self):
-        return f"""{self.magna_price} cuesta la magna en {self.station_id}
-                {self.premium_price} cuesta la premium en {self.station_id}
-                {self.diesel_price} cuesta el diesel en {self.station_id}
-        """
-
-
-
-class Users(models.Model):
-    user = models.OneToOneField(User, on_delete=models.PROTECT)
-
-    def __str__(self):
-        return self
-
-class Complaints(models.Model):
-    user_id = models.ForeignKey(Users, on_delete=models.PROTECT)
-
-    date = models.DateField(auto_now_add=True)
-    description = models.CharField(max_length=255)
-    link_evidence = models.CharField(max_length=255)
-    type_complaint = models.CharField(max_length=255)
+    date = models.DateTimeField(
+        'created at',
+        auto_now=True
+        help_text='Date Time on wich the prices are registred'
+        )
+    magna_price = models.FloatField(
+        max_length=5,
+        null=True
+        )
+    premium_price = models.FloatField(
+        max_length=5,
+        null=True
+        )
+    diesel_price = models.FloatField(
+        max_length=5,
+        null=True
+        )
 
     def __str__(self):
-        return f'{self.user_id} en {self.date}'
-
+        """ Returns price of magna."""
+        return f'{self.magna_price} cuesta la magna en {self.station}'
 """
 class GasTypes(models.Model):
     stations = models.ForeignKey(Stations, on_delete=models.PROTECT)
@@ -57,10 +61,41 @@ class GasTypes(models.Model):
 
     def __str__(self):
         return self.gasType
-"""     
-
-
+"""        
+class Profile(models.Model):
+    """Profile Model 
     
+    Model that extends the user model and add extra information of the user.
+    """
 
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+
+    def __str__(self):
+        """Returns string representation of user"""
+        return self
+
+class Complaints(models.Model):
+    """Complaints Model
+
+    A complaint is a bad calification that an user makes 
+    about the prices in a special station.
+    """
+    user = models.ForeignKey(
+        Users, 
+        on_delete=models.PROTECT
+    )
+    date = models.DateTimeField(
+        auto_now_add=True,
+        help_text='Datetime when the complaint is created')
+    description = models.TextField(
+        max_length=500,
+        null=False,
+        )
+    link_evidence = models.CharField(max_length=255)
+    type_complaint = models.CharField(max_length=255)
+
+    def __str__(self):
+        """ Returns usern and datetime of the complaint."""
+        return f'{self.user} en {self.date}'
 
 
